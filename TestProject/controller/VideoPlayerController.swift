9e006ce86b55c78599bb07bae3ea8aad397f2280
wy353208214 <wangyangbayern@gmail.com>
2021-09-24 16:22:03 +0800

    添加播发时间

--- TestProject/controller/VideoPlayerController.swift
+++ TestProject/controller/VideoPlayerController.swift
@@ -14,9 +14,15 @@ import CoreMedia
 class VideoPlayerController: UIViewController {
 
     private var movie: MovieModel?
-    fileprivate var videoPlayer = Player()    
-    private var progressView = UISlider()
-
+    
+    fileprivate var videoPlayer = Player()
+    
+    private let progressContent = UIView.init(frame: .zero)
+    private let progressView = UISlider()
+    private let lengthLabel = UILabel()
+    private let currentTimeLabel = UILabel()
+    
+    
     deinit {
         self.videoPlayer.willMove(toParent: nil)
         self.videoPlayer.view.removeFromSuperview()
@@ -38,16 +44,36 @@ class VideoPlayerController: UIViewController {
         //禁用左滑返回手势，防止slide无法拖动
         self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
         
+        //设置播放状态监听
         self.videoPlayer.playerDelegate = self
         self.videoPlayer.playbackDelegate = self
         self.videoPlayer.playerView.playerBackgroundColor = .white
         
-        progressView.backgroundColor = UIColor.systemGray
-        progressView.minimumTrackTintColor = UIColor.systemGreen
+        //设置播放器控制条
+        progressView.maximumTrackTintColor = .white
+        progressView.minimumTrackTintColor = .systemGreen
+        
+        lengthLabel.textColor = .white
+        lengthLabel.font = .systemFont(ofSize: 12)
+        lengthLabel.numberOfLines = 1
+        lengthLabel.preferredMaxLayoutWidth = 20
+        
+        currentTimeLabel.textColor = .white
+        currentTimeLabel.font = .systemFont(ofSize: 12)
+        currentTimeLabel.numberOfLines = 1
+        currentTimeLabel.preferredMaxLayoutWidth = 20
+        
+        progressContent.backgroundColor = UIColor.init(white: 0, alpha: 0.4)
+        
         
         addChild(self.videoPlayer)
         view.addSubview(self.videoPlayer.view)
-        view.addSubview(progressView)
+        
+        //添加底部控制器
+        progressContent.addSubview(progressView)
+        progressContent.addSubview(lengthLabel)
+        progressContent.addSubview(currentTimeLabel)
+        view.addSubview(progressContent)
         
         self.videoPlayer.didMove(toParent: self)
         self.videoPlayer.url = URL(string: movie!.mp4_url)
@@ -65,12 +91,27 @@ class VideoPlayerController: UIViewController {
             make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
             make.bottom.equalToSuperview()
         }
-        progressView.snp.makeConstraints{(make) -> Void in
-            make.bottom.equalTo(videoPlayer.view.snp.bottom).offset(-20)
-            make.width.equalToSuperview()
-            make.height.equalTo(2)
-        }
         
+        progressContent.snp.makeConstraints{(make) -> Void in
+            make.bottom.equalTo(videoPlayer.view.snp.bottom)
+            make.width.equalToSuperview()
+            make.height.equalTo(60)
+        }
+        lengthLabel.snp.makeConstraints{(make) -> Void in
+            make.centerY.equalToSuperview()
+            make.right.equalToSuperview().offset(-2)
+        }
+        currentTimeLabel.snp.makeConstraints{(make) -> Void in
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().offset(2)
+        }
+        progressView.snp.makeConstraints{(make) -> Void in
+            make.centerY.equalToSuperview()
+            make.left.equalTo(currentTimeLabel.snp.right).offset(2)
+            make.right.equalTo(lengthLabel.snp.left).offset(-2)
+            make.height.equalTo(4)
+        }
+        progressContent.isHidden = true
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -81,8 +122,12 @@ class VideoPlayerController: UIViewController {
     
     func formateHSM(second: Int) -> String {
         let formatter = DateComponentsFormatter.init()
-        formatter.zeroFormattingBehavior = .dropAll
-        formatter.allowedUnits = NSCalendar.Unit(rawValue: NSCalendar.Unit.hour.rawValue | NSCalendar.Unit.minute.rawValue | NSCalendar.Unit.second.rawValue)
+        formatter.zeroFormattingBehavior = .pad
+        if second >= 3600 {
+            formatter.allowedUnits = NSCalendar.Unit(rawValue: NSCalendar.Unit.hour.rawValue | NSCalendar.Unit.minute.rawValue | NSCalendar.Unit.second.rawValue)
+        }else {
+            formatter.allowedUnits = NSCalendar.Unit(rawValue: NSCalendar.Unit.minute.rawValue | NSCalendar.Unit.second.rawValue)
+        }
         formatter.unitsStyle = DateComponentsFormatter.UnitsStyle.positional
         let resultStr = formatter.string(from: TimeInterval.init(second))
         return resultStr!
@@ -90,7 +135,9 @@ class VideoPlayerController: UIViewController {
     
     @objc func sliderValueChanged(slider: UISlider) {
         //拖动到指定时间
-        self.videoPlayer.seek(to: CMTimeMakeWithSeconds(Float64(slider.value), preferredTimescale: 1))
+        let cmt = CMTimeMake(value: Int64(slider.value), timescale:1)
+//        print(cmt.value)
+        self.videoPlayer.seek(to: cmt)
     }
     
 }
@@ -120,10 +167,13 @@ extension VideoPlayerController {
 extension VideoPlayerController: PlayerDelegate {
     
     func playerReady(_ player: Player) {
-        print("\(#function) ready")
         let videoDuration = Int(player.maximumDuration)
         print("视频时长是：\(videoDuration)秒, 格式化：\(formateHSM(second: videoDuration))")
+        
         self.progressView.maximumValue = Float(videoDuration)
+        lengthLabel.text = formateHSM(second: videoDuration)
+        
+        progressContent.isHidden = false
     }
     
     func playerPlaybackStateDidChange(_ player: Player) {
@@ -159,6 +209,7 @@ extension VideoPlayerController: PlayerPlaybackDelegate {
 
     public func playerCurrentTimeDidChange(_ player: Player) {
         self.progressView.setValue(Float(player.currentTimeInterval) , animated: false)
+        currentTimeLabel.text = formateHSM(second: Int(player.currentTimeInterval))
     }
 
     public func playerPlaybackWillLoop(_ player: Player) {
